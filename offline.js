2ef4f8f2c96eb15d11e5b4fe7ca69d482a6834ee
prikolsite <99741695+prikolsite@users.noreply.github.com>
2022-02-15 19:25:48 +0500

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1644934579,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/piupau-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/direction-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/camera_target-sheet0.png",
+		"images/camera-sheet0.png",
+		"images/bloodeffect.png",
+		"images/deadbody-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"Photon-Javascript_SDK.min.js"
+	]
+}
